2735b36034fbe7c28b352c37a9fe9890402eb03b
Sara Bartman <sara.bartman.05@gmail.com>
2024-10-13 10:38:10 +0200

    perft arguments change

--- src/perft.cpp
+++ src/perft.cpp
@@ -10,7 +10,7 @@ int board::perft(int depth, int org_depth) {
         auto move = move_from_pair(moves.top());
         moves.pop();
         make_move(move, false);
-        auto perft_res = perft(depth-1);
+        auto perft_res = perft(depth-1, org_depth);
         if(depth == org_depth)
             cout << perft_res << ' ' << move.start_pos << ' ' << move.end_pos << '\n';
         res += perft_res;
